418d088cf4b37ed1eb6fe1936f6cd92b64183141
marshki <marshall@morethantomorrow.org>
2019-11-29 10:13:04 -0500

    redo, need to do this to effectively debug

--- ipypass/gui.py
+++ ipypass/gui.py
@@ -28,51 +28,14 @@ class MainWindow(tk.Frame):
         tk.Frame.__init__(self, master)
         master.config(menu=MenuBar(self))
 
-        self.mode = 'eightbit'
-
-        self.label_in = tk.Label(self, text='Enter IP address:')
-        self.label_in.pack()
-
-        self.ip_in = tk.DoubleVar(0)
-        text = tk.Entry(self, textvar=self.ip_in)
-        text.pack()
-
-        self.ip_out = tk.StringVar(self, '0*8*****')
-        lbl = tk.Label(self, textvar=self.ip_out)
-        lbl.pack()
-
-        btn = tk.Button(self, text='Convert', command=self.convert)
-        btn.pack()
-
-        btn = tk.Button(self, text='Quit', command=master.destroy)
-        btn.pack()
-
-    def eight_bit_mode(self):
-        self.mode = 'eightbit'
-        self.label_in.config(text='8-bit password:')
-
-    def twelve_bit_mode(self):
-        self.mode = 'twelvebit'
-        self.label_in.config(text='12-bit password:') 
-
-    def convert(self):
-        try:
-            if self.mode == 'eightbit':
-                ip_out = eight_bit_passwd(self.ip_in.get())
-            else:
-                ip_out = twelve_bit_passwd(self.temp_in.get())
-            self.ip_out.set("{:.3f}".format(ip_out))
-        except ValueError as e:
-            self.ip_out.set("ERROR: {}".format(e))
-
 class MenuBar(tk.Menu):
     def __init__(self, master):
         tk.Menu.__init__(self, master)
 
         file_menu = tk.Menu(self, tearoff=False)
         self.add_cascade(label='File', menu=file_menu)
-        file_menu.add_command(label='8 bit mode', command=master.eight_bit_mode)
-        file_menu.add_command(label='12 bit mode', command=master.twelve_bit_mode)
+        #file_menu.add_command(label='F to C mode', command=master.f2c_mode)
+        #file_menu.add_command(label='C to F mode', command=master.c2f_mode)
         file_menu.add_separator()
         file_menu.add_command(label='Exit', command=self.quit)
 
