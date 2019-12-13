f45e9c853e7169979a45d9e83d0704e7fcb4b407
marshki <marshall@morethantomorrow.org>
2019-12-13 15:18:57 -0500

    kinda working

--- ipypass/test.py
+++ ipypass/test.py
@@ -1,22 +1,34 @@
-import PySimpleGUI as sg      
+#!/usr/bin/env python3
 
-layout = [ [sg.Txt('Enter IP address:')],      
-            [sg.In(size=(8,1), key='eight_bit')],      
-            [sg.Txt('', size=(8,1), key='output')  ],      
-            [sg.Button('Convert', bind_return_key=True)]]      
+import PySimpleGUI as sg
 
-window = sg.Window('IPyPass', layout)      
+sg.change_look_and_feel('Reddit')
 
-while True:      
-    event, values = window.read()      
+LAYOUT = [[sg.Txt('Enter IP address:')],
+          [sg.In(size=(16, 1), key='IP_ADDRESS')],
+          [sg.Txt('', size=(16, 1), key='output')],
+          [sg.Button('Convert', bind_return_key=True)]]
 
-    if event is not None:      
-        try:      
-            eight_bit = str(values['eight_bit'])      
-            calc = eight_bit  
-        except:      
-            calc = 'Invalid'      
+window = sg.Window('IPyPass', LAYOUT)
 
-        window['output'].update(calc)      
-    else:      
-        break      
+while True:
+    EVENT, VALUES = window.read()
+
+    if EVENT is not None:
+        try:
+            IP_ADDRESS = str(VALUES['IP_ADDRESS'])
+            SPLIT_ADDRESS = IP_ADDRESS.split('.', 4)
+
+            def eight_bit_passwd():
+                """Transform IP address to 8-bit password.
+                """
+                return((SPLIT_ADDRESS[2] + '*' +
+                    str(int(SPLIT_ADDRESS[3]) + 8)).ljust(8, '*'))
+
+            CALC = eight_bit_passwd()
+        except:
+            CALC = 'Invalid'
+
+        window['output'].update(CALC)
+    else:
+        break
