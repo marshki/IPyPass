19ff9a1fcaa8df960041ad325a8a7073f58a3dc2
marshki <marshall@morethantomorrow.org>
2019-12-13 20:25:38 -0500

    add main function

--- ipypass/test.py
+++ ipypass/test.py
@@ -15,35 +15,43 @@ LAYOUT = [[sg.Txt('Enter IP address:')],
 
 window = sg.Window('IPyPass', LAYOUT)
 
-while True:
-    EVENT, VALUES = window.read()
+def ipypass():
+    """Guts.
+    """
 
-    if EVENT is not None:
-        try:
-            IP_ADDRESS = str(VALUES['IP_ADDRESS'])
-            SPLIT_ADDRESS = IP_ADDRESS.split('.', 4)
+    while True:
+        EVENT, VALUES = window.read()
 
-            def eight_bit_passwd():
-                """Transform IP address to 8-bit password.
-                """
-                return((SPLIT_ADDRESS[2] + '*' +
-                        str(int(SPLIT_ADDRESS[3]) + 8)).ljust(8, '*'))
+        if EVENT is not None:
+            try:
+                IP_ADDRESS = str(VALUES['IP_ADDRESS'])
+                SPLIT_ADDRESS = IP_ADDRESS.split('.', 4)
 
-            def twelve_bit_passwd():
-                """Transform IP address to 12-bit password.
-                """
-                return((SPLIT_ADDRESS[2] + '*' +
-                        str(int(SPLIT_ADDRESS[3]) + 12) + '*' + SPLIT_ADDRESS[1]).ljust(12, '*'))
+                def eight_bit_passwd():
+                    """Transform IP address to 8-bit password.
+                    """
+                    return((SPLIT_ADDRESS[2] + '*' +
+                            str(int(SPLIT_ADDRESS[3]) + 8)).ljust(8, '*'))
 
-            EIGHT_BIT = eight_bit_passwd()
-            TWELVE_BIT = twelve_bit_passwd()
+                def twelve_bit_passwd():
+                    """Transform IP address to 12-bit password.
+                    """
+                    return((SPLIT_ADDRESS[2] + '*' +
+                            str(int(SPLIT_ADDRESS[3]) + 12) + '*' 
+                            + SPLIT_ADDRESS[1]).ljust(12, '*'))
 
-        except Exception as e:
-            EIGHT_BIT = 'Invlid input.'
-            TWELVE_BIT = 'Try again.'
+                EIGHT_BIT = eight_bit_passwd()
+                TWELVE_BIT = twelve_bit_passwd()
 
-        window['output_1'].update(EIGHT_BIT)
-        window['output_2'].update(TWELVE_BIT)
+            except Exception as e:
+                EIGHT_BIT = 'Invlid input.'
+                TWELVE_BIT = 'Try again.'
 
-    else:
-        break
+            window['output_1'].update(EIGHT_BIT)
+            window['output_2'].update(TWELVE_BIT)
+
+        else:
+            break
+
+if __name__ == '__main__':
+    ipypass()
