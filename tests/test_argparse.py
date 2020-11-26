964df943757f060d81649a985d67da5c8bc602ce
marshki <marshall@morethantomorrow.org>
2020-11-26 12:11:09 -0500

    update, but not working

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """Argparser.
 """
 
 import argparse
+import unittest
+
+from unittest.mock import patch
 
 def parse_cli_args():
     """Command line parser for subnet of interest.
@@ -23,9 +26,20 @@ def parse_cli_args():
 
     return args
 
+class ParseCLITest(unittest.TestCase):
+
+    """Unit tests.
+    Use `patch()` to mock objects for testing.
+    For reference: https://docs.python.org/3/library/unittest.mock.html
+    """
+
+    @patch('builtins.input', return_value='--ip 192.168.1.1')
+    def test_parse_cli_args_01(self, input):
+        """Valid return value.
+        """
+        self.assertEqual(parse_cli_args(), '192.168.1.1')
+
 if __name__ == '__main__':
     args = parse_cli_args()
-    print(args.ip)
-
-
-# TODO: Add unit test. Fix linter issues.
+    unittest.main()
+    #print(args.ip)
