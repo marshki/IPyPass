4d252876b54918df4ebd41a8c83b26e55622bde3
marshki <marshall@morethantomorrow.org>
2020-11-27 10:35:25 -0500

    fix linter issues

--- tests/test_ipypass.py
+++ tests/test_ipypass.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """Convert IPv4 address to 8- or 12-bit password.
+
 8-bit algorithm:
 Take 4-octet IPv4 address -->
 split address by '.' -->
@@ -26,30 +27,25 @@ ADDRESS = '192.168.1.1'
 SPLIT_ADDRESS = ADDRESS.split('.', 4)
 
 def eight_bit_passwd():
-    """Transform IP address to 8-bit password.
-    """
+    """Transform IP address to 8-bit password."""
     return((SPLIT_ADDRESS[2] + '*' +
             str(int(SPLIT_ADDRESS[3]) + 8)).ljust(8, '*'))
 
 def twelve_bit_passwd():
-    """Transform IP address to 12-bit password.
-    """
+    """Transform IP address to 12-bit password."""
     return((SPLIT_ADDRESS[2] + '*' +
             str(int(SPLIT_ADDRESS[3]) + 12) + '*' + SPLIT_ADDRESS[1]).ljust(12, '*'))
 
 class SplitIPTest(unittest.TestCase):
-    """Unit tests.
-    """
+    """Unit tests."""
 
     def test_eight_bit_passwd_01(self):
-        """Valid return value.
-        """
+        """Valid return value."""
         self.assertEqual(eight_bit_passwd(), '1*9*****')
 
     @unittest.expectedFailure
     def test_eight_bit_passwd_02(self):
-        """Invalid return value.
-        """
+        """Invalid return value."""
         self.assertEqual(eight_bit_passwd(), '1*13*168****')
 
     def test_twelve_bit_passwd_01(self):
@@ -59,8 +55,7 @@ class SplitIPTest(unittest.TestCase):
 
     @unittest.expectedFailure
     def test_twelve_bit_passwd_02(self):
-        """Invalid return value.
-        """
+        """Invalid return value."""
         self.assertEqual(twelve_bit_passwd(), '1*9*****')
 
 if __name__ == '__main__':
