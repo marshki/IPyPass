3afe56deba9b7bbd5394bf7cdab3018025009fca
marshki <marshall@morethantomorrow.org>
2020-11-28 10:33:02 -0500

    linter cleanup

--- tests/test_validate_ip.py
+++ tests/test_validate_ip.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # pylint: disable=W0613,W0622
 
-"""IPv4 validation using `ipaddress` module.
+"""
+IPv4 validation using `ipaddress` module.
 
 A ValueError is raised if address does not represent a valid IPv4.
 For reference: https://docs.python.org/3/library/ipaddress.html
@@ -14,7 +15,6 @@ from unittest.mock import patch
 
 def ipv4_addr_check():
     """Prompt user for IPv4 address, then validate. Re-prompt if invalid."""
-
     while True:
         try:
             return ipaddress.IPv4Address(input('Enter valid IPv4 address: '))
@@ -27,7 +27,6 @@ class IPv4AddrCheckTest(unittest.TestCase):
     Use `patch()` to mock objects for testing.
     For reference: https://docs.python.org/3/library/unittest.mock.html
     """
-
     @patch('builtins.input', return_value='192.168.1.1')
     def test_ipv4_addr_check_01(self, input):
         """Valid return value."""
