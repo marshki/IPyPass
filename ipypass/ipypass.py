21b7a05cbaeae29c1eb35f88e343e2f6bb91235b
marshki <marshall@morethantomorrow.org>
2020-11-30 09:36:18 -0500

    add argparse, overwrite older, flawed iteration

--- ipypass/ipypass.py
+++ ipypass/ipypass.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
-"""Take IPv4 addresss and return 8- or 12-bit password (CLI)."""
+"""Take IPv4 addresss and return 8-bit, 12-bit passwords."""
 
 import ipaddress
+import argparse
+
+def parse_cli_args():
+    """
+    Command line parser for IPv4 address of interest.
+
+    Args: IPv4 address, e.g.: --ip 0.0.0.0
+    Returns: String, e.g.: 0.0.0.0
+
+    """
+
+    parser = argparse.ArgumentParser(description="IPv4 address of interest.")
+    parser.add_argument("--ip", action="store", type=str,\
+                        help="IP address of interest, e.g. 0.0.0.0")
+    args = parser.parse_args()
+
+    return args
 
 def ipv4_addr_check():
-    """Validate IPv4 address.
+    """
+    Validate IPv4 address.
 
-    Args: IPv4 address of the form: 0.0.0.0
-    Returns: String-converted IPv4 address.
+    Args: IPv4 address, e.g.: 0.0.0.0
+    Returns: String-converted IPv4 address, e.g.: 0.0.0.0
     Raises: ValueError.
 
     """
@@ -19,7 +37,8 @@ def ipv4_addr_check():
             print('Bad value, try again.')
 
 def eight_bit_passwd():
-    """Transform IP address to 8-bit password.
+    """
+    Transform IP address to 8-bit password.
 
     Args: String-converted IPv4 address, split by octet.
     Returns: 8-bit password.
@@ -29,7 +48,8 @@ def eight_bit_passwd():
             str(int(SPLIT_ADDRESS[3]) + 8)).ljust(8, '*'))
 
 def twelve_bit_passwd():
-    """Transform IP address to 12-bit password.
+    """
+    Transform IP address to 12-bit password.
 
     Args: String-converted IPv4 address, split by octet.
     Returns: 12-bit password.
@@ -38,9 +58,26 @@ def twelve_bit_passwd():
     return((SPLIT_ADDRESS[2] + '*' +
             str(int(SPLIT_ADDRESS[3]) + 12) + '*' + SPLIT_ADDRESS[1]).ljust(12, '*'))
 
-if __name__ == '__main__':
-    ADDRESS = ipv4_addr_check()
-    SPLIT_ADDRESS = ADDRESS.split('.', 4)
+def create_table():
+    """
+    Create a crude table.
+
+    Returns: Table with transformed output.
+
+    """
+
     print('8-bit   ', '12-bit     ')
     print('--------', '------------')
     print(eight_bit_passwd(), twelve_bit_passwd())
+
+if __name__ == '__main__':
+    args = parse_cli_args()
+
+    if args.ip:
+        ADDRESS = args.ip
+        SPLIT_ADDRESS = ADDRESS.split('.', 4)
+        create_table()
+    else:
+        ADDRESS = ipv4_addr_check()
+        SPLIT_ADDRESS = ADDRESS.split('.', 4)
+        create_table()
