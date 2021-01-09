65968c43a1943dc82f84d36f9b4d1eac965f9b63
marshki <marshall@morethantomorrow.org>
2021-01-09 14:15:39 -0500

    add, needs more unit tests

---
+++ tests/test_argparse_ip.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""IPv4 validation using `ipaddress module` and argparse."""
+
+import argparse
+from ipaddress import ip_address
+
+import unittest
+
+def parse_cli_args():
+    """
+    Command line parser for subnet of interest.
+    Args:
+      --ip 0.0.0.0
+    Returns:
+         String, e.g. 0.0.0.0
+    """
+
+    parser = argparse.ArgumentParser(description="IPv4 address of interest.")
+    parser.add_argument("--ip", action="store",\
+                        required=True,\
+                        help="IP address of interest, e.g. 0.0.0.0")
+
+    return parser
+
+class ParseCLIArgs(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = parse_cli_args()
+
+    def test_parser_cli_args(self):
+        parsed = self.parser.parse_args(['--ip', '192.168.1.1'])
+        self.assertEqual(parsed.ip, '192.168.1.1')
+
+if __name__ == '__main__':
+    unittest.main()
