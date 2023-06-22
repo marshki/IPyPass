33b94522ef07a04b1d6b68e61f4f5cf2d5040313
marshki <marshall@morethantomorrow.org>
2023-06-22 16:55:47 -0400

    add refactored unit tests

--- derp.py
+++ derp.py
@@ -9,6 +9,19 @@ import unittest
 from ipaddress import IPv4Address
 from unittest.mock import patch
 
+import logging
+import PySimpleGUI as sg
+
+sg.change_look_and_feel('Reddit')
+
+LAYOUT = [[sg.Txt('Enter IP address:')],
+          [sg.In(size=(16, 1), key='ip_address')],
+          [sg.Txt('', size=(16, 1), key='output_1')],
+          [sg.Txt('', size=(16, 1), key='output_2')],
+          [sg.Button('Convert', bind_return_key=True)]]
+
+Window = sg.Window('IPyPass', LAYOUT)
+
 def parse_cli_args():
     """
     Command line parser for IPv4 address of interest, with validation.
@@ -28,6 +41,33 @@ def parse_cli_args():
 
     return args.ip
 
+def eight_bit_passwd(split_address):
+    """
+    Transform IP address to 8-bit password.
+
+    Args:
+        split_address: List containing the four octets of the IP address.
+
+    Returns:
+        8-bit password.
+    """
+    return (split_address[2] + '*' +
+            str(int(split_address[3]) + 8)).ljust(8, '*')
+
+def twelve_bit_passwd(split_address):
+    """
+    Transform IP address to 12-bit password.
+
+    Args:
+        split_address: List containing the four octets of the IP address.
+
+    Returns:
+        12-bit password.
+    """
+    return (split_address[2] + '*' +
+            str(int(split_address[3]) + 12) + '*' +
+            split_address[1]).ljust(12, '*')
+
 class IPyPassTests(unittest.TestCase):
     """Unit tests."""
 
@@ -41,6 +81,23 @@ class IPyPassTests(unittest.TestCase):
     def test_parse_cli_args_no_ip(self):
         ip = parse_cli_args()
         self.assertIsNone(ip)
+
+    def test_eight_bit_passwd(self):
+        split_address = ['192', '168', '0', '1']
+        password = eight_bit_passwd(split_address)
+        self.assertEqual(password, '0*9*****')
+
+    def test_twelve_bit_passwd(self):
+        split_address = ['192', '168', '0', '1']
+        password = twelve_bit_passwd(split_address)
+        self.assertEqual(password, '0*13*168****')
+
+    def test_create_table(self):
+        ip_address = IPv4Address('192.168.0.1')
+        expected_output = '8-bit    12-bit     \n-------- ------------\n0*9***** 0*13*168****\n'
+        with redirect_stdout(StringIO()) as output:
+            create_table(ip_address)
+            self.assertEqual(output.getvalue(), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
