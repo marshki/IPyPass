2aebc6aac6ff10df6c70658d3b7845cb56a4b4fc
marshki <marshall@morethantomorrow.org>
2023-08-19 15:14:01 -0400

    update

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -15,28 +15,36 @@ class IPyPassTests(unittest.TestCase):
     """
 
     def test_parse_cli_args(self):
-        # Test with valid IP address
+        """Test with valid IP address.
+        """
         args = ['--ip', '192.168.0.1']
         ip = parse_cli_args(args)
         self.assertIsInstance(ip, IPv4Address)
         self.assertEqual(str(ip), '192.168.0.1')
 
-        # Test without IP address
+        """Test without IP address.
+        """
         args = []
         ip = parse_cli_args(args)
         self.assertIsNone(ip)
 
     def test_eight_bit_passwd(self):
+        """Test valid return value.
+        """
         split_address = ['192', '168', '0', '1']
         password = eight_bit_passwd(split_address)
         self.assertEqual(password, '0*9*****')
 
     def test_twelve_bit_passwd(self):
+        """Test valid return value.
+        """
         split_address = ['192', '168', '0', '1']
         password = twelve_bit_passwd(split_address)
         self.assertEqual(password, '0*13*168****')
 
     def test_create_table(self):
+        """Test talbe creation.
+        """
         ip_address = IPv4Address('192.168.0.1')
         expected_output = '8-bit    12-bit     \n-------- ------------\n0*9***** 0*13*168****\n'
         with redirect_stdout(StringIO()) as output:
